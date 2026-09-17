1372f4c4a4e49873c7fba5e54ae59355aedbf59a
Anthony Prosser <anthony.c.prosser@gmail.com>
2026-09-16 20:11:51 -0500

    I created the instructions for the practice problem.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PracticeProblem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PracticeProblem.java
@@ -1,7 +1,27 @@
 package org.firstinspires.ftc.teamcode;
 
+/*
+Task for the method `strStr`:
+Given two strings `needle` and `haystack`, return the index of the first occurrence of `needle` in
+`haystack`, or -1 if needle is not part of haystack.
+
+Example 1:
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+Explanation: "sad" occurs at index 0 and 6.
+The first occurrence is at index 0, so we return 0.
+
+Example 2:
+Input: haystack = "firemen", needle = "water"
+Output: -1
+Explanation: "water" did not occur in "firemen", so we return -1.
+
+haystack and needle consist of only lowercase English characters.
+ */
 public class PracticeProblem {
     public static int strStr(String haystack, String needle) {
-        return 0;
+        //Implement Here
+
+        return -1;
     }
 }
